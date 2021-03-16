7777b19f9de45bf5731256be7c4d62b03768c04c
Dennis Jauernig <10451521+dride-for-sure@users.noreply.github.com>
2021-03-16 19:35:52 +0100

    21-03-16 Init Project (#55)
    * :tada: Init backend
    
    * :tada: Init Frontend, add Eslint, Prettier and enforce AirBnB Styleguide

---
+++ backend/src/main/java/org/opensource/schulaltas/SchulaltasApplication.java
@@ -0,0 +1,13 @@
+package org.opensource.schulaltas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchulaltasApplication {
+
+ public static void main (String[] args) {
+  SpringApplication.run( SchulaltasApplication.class, args );
+ }
+
+}
