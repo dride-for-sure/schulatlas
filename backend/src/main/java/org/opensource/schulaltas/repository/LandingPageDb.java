8fb24499f702d404cf17648b3f906f3e7a7ec83c
dennisjauernig <dennis@jauernig.email>
2021-03-17 09:52:23 +0100

    :fire: Remove not necessary db

--- backend/src/main/java/org/opensource/schulaltas/repository/LandingPageDb.java
+++
@@ -1,9 +0,0 @@
-package org.opensource.schulaltas.repository;
-
-import org.opensource.schulaltas.model.page.LandingPage;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LandingPageDb extends PagingAndSortingRepository<LandingPage, String> {
-}
