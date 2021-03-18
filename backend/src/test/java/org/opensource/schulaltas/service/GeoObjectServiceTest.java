05b85bf663416a404ce3922538ac49f97c45beab
dennisjauernig <dennis@jauernig.email>
2021-03-18 10:57:13 +0100

    :white_check_mark: Add exception test for google geo api

--- backend/src/test/java/org/opensource/schulaltas/service/GeoObjectServiceTest.java
+++ backend/src/test/java/org/opensource/schulaltas/service/GeoObjectServiceTest.java
@@ -8,6 +8,7 @@ import org.opensource.schulaltas.model.school.Address;
 import org.opensource.schulaltas.model.school.Coordinates;
 import org.opensource.schulaltas.model.school.GeoObject;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -73,6 +74,38 @@ class GeoObjectServiceTest {
                                                                 .longitude( 2.0 )
                                                                 .build() ).build();
   assertThat( actual.get(), is( expected ) );
+  verify( restTemplate ).getForEntity( base_url + extension, GeoResultsDto.class );
+ }
+
+ @Test
+ @DisplayName ("Get coordinates should return an optional.empty if google geo api response throws" +
+                       " an exception")
+ void getCoordinatesFromInvalidAddress () {
+  // GIVEN
+  Address address = Address.builder()
+                            .city( "A" )
+                            .number( "1" )
+                            .street( "B" )
+                            .postcode( "1" )
+                            .build();
+  String key = "key";
+  String street = address.getStreet();
+  String number = address.getNumber();
+  String postcode = address.getPostcode();
+  String city = address.getCity();
+  String country = "DE";
+  String base_url = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+  String extension = street + "+" + number + ",+" + postcode + "+" + city + ",+" + country + "&key=" + key;
+
+  when( googleGeoConfig.getKey() ).thenReturn( key );
+  when( restTemplate.getForEntity( base_url + extension, GeoResultsDto.class ) )
+          .thenThrow( RestClientException.class );
+
+  // WHEN
+  Optional<GeoObject> actual = geoService.getCoordinatesFromAddress( address );
+
+  // THEN
+  assertThat( actual.isEmpty(), is( true ) );
   verify( restTemplate ).getForEntity( base_url + extension, GeoResultsDto.class );
  }
 }
