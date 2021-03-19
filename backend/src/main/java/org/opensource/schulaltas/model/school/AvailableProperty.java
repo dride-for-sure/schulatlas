b2524ae767c1627abeb676588228f333b271ebd2
dennisjauernig <dennis@jauernig.email>
2021-03-19 17:32:23 +0100

    :card_file_box: Add @Id annotation to availableProperty model

--- backend/src/main/java/org/opensource/schulaltas/model/school/AvailableProperty.java
+++ backend/src/main/java/org/opensource/schulaltas/model/school/AvailableProperty.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
@@ -13,6 +14,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document (collection = "AvailableProperties")
 public class AvailableProperty {
 
+ @Id
  private String name;
  private String unit;
 }
