254f13d2b57ae87d2943991cee2f051ef77cd73a
dennisjauernig <dennis@jauernig.email>
2021-03-20 21:51:37 +0100

    :white_check_mark: Shorten the tests

--- backend/src/test/java/org/opensource/schulaltas/security/AuthenticateControllerTest.java
+++ backend/src/test/java/org/opensource/schulaltas/security/AuthenticateControllerTest.java
@@ -2,12 +2,14 @@ package org.opensource.schulaltas.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.opensource.schulaltas.repository.SchoolUserDb;
 import org.opensource.schulaltas.security.model.AuthenticationRequest;
 import org.opensource.schulaltas.security.model.SchoolUser;
 import org.opensource.schulaltas.security.model.enums.SchoolUserAuthorities;
+import org.opensource.schulaltas.service.TimeInstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -17,6 +19,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
 
+import java.sql.Date;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -30,6 +35,9 @@ class AuthenticateControllerTest {
  private int port;
 
  @Autowired
+ private TimeInstant timeInstant;
+
+ @Autowired
  private SchoolUserDb schoolUserDb;
 
  @Autowired
@@ -38,6 +46,18 @@ class AuthenticateControllerTest {
  @Autowired
  private TestRestTemplate testRestTemplate;
 
+ @BeforeEach
+ public void Setup () {
+  String testUser = "testUser";
+  String testPassword = "testPassword";
+  String encodedPassword = passwordEncoder.encode( testPassword );
+  schoolUserDb.save( SchoolUser.builder()
+                             .username( testUser )
+                             .password( encodedPassword )
+                             .authorities( List.of( SchoolUserAuthorities.ADMIN ) )
+                             .build() );
+ }
+
  private String getUrl () {
   return "http://localhost:" + port + "authenticate";
  }
@@ -45,42 +65,24 @@ class AuthenticateControllerTest {
  @Test
  @DisplayName ("Create authentication token should generate a valid token valid credentials")
  void createAuthenticationToken () {
-  // GIVEN
-  String testUser = "testUser";
-  String testPassword = "testPassword";
-  String encodedPassword = passwordEncoder.encode( testPassword );
-  schoolUserDb.save( SchoolUser.builder()
-                             .username( testUser )
-                             .password( encodedPassword )
-                             .authorities( List.of( SchoolUserAuthorities.ADMIN ) )
-                             .build() );
-
   // WHEN
   AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
-                                                        .username( testUser )
-                                                        .password( testPassword )
+                                                        .username( "testUser" )
+                                                        .password( "testPassword" )
                                                         .build();
   ResponseEntity<String> response = testRestTemplate.postForEntity( getUrl(), authenticationRequest, String.class );
 
   // THEN
+  Instant nowInstant = Instant.ofEpochSecond( Instant.now().getEpochSecond() );
   Claims claims = Jwts.parser().setSigningKey( "testsecret" ).parseClaimsJws( response.getBody() ).getBody();
   assertThat( response.getStatusCode(), is( HttpStatus.OK ) );
   assertThat( claims.getSubject(), is( "testUser" ) );
+  assertThat( claims.getExpiration(), is( Date.from( nowInstant.plus( Duration.ofHours( 12 ) ) ) ) );
  }
 
  @Test
  @DisplayName ("Create authentication token should throw an exception for invalid credentials")
  void createAuthenticationTokenThrowsException () {
-  // GIVEN
-  String testUser = "testUser";
-  String testPassword = "testPassword";
-  String encodedPassword = passwordEncoder.encode( testPassword );
-  schoolUserDb.save( SchoolUser.builder()
-                             .username( testUser )
-                             .password( encodedPassword )
-                             .authorities( List.of( SchoolUserAuthorities.ADMIN ) )
-                             .build() );
-
   // WHEN
   AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
                                                         .username( "anotherTestUser" )
