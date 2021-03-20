c1ff71808edd36c48f6ab3d96ac6053a2f6c0baf
dennisjauernig <dennis@jauernig.email>
2021-03-20 20:26:45 +0100

    :sparkles: Implement S3 upload

---
+++ backend/src/main/java/org/opensource/schulaltas/service/AWSService.java
@@ -0,0 +1,76 @@
+package org.opensource.schulaltas.service;
+
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+public class AWSService {
+
+ private String awsS3AudioBucket;
+ private AmazonS3 amazonS3;
+ private static final Logger logger = LoggerFactory.getLogger( AWSService.class );
+
+ @Autowired
+ public AWSService (Region awsRegion,
+                    AWSCredentialsProvider awsCredentialsProvider,
+                    String awsS3AudioBucket) {
+  amazonS3 = AmazonS3ClientBuilder.standard()
+                     .withCredentials( awsCredentialsProvider )
+                     .withRegion( awsRegion.getName() ).build();
+  this.awsS3AudioBucket = awsS3AudioBucket;
+ }
+
+ @Async
+ public Optional<MultipartFile> uploadFileToS3 (MultipartFile multipartFile,
+                                                boolean enablePublicReadAccess) {
+  String fileName = multipartFile.getOriginalFilename();
+  try {
+   File file = new File( fileName );
+   FileOutputStream fileOutputStream = new FileOutputStream( file );
+   fileOutputStream.write( multipartFile.getBytes() );
+   fileOutputStream.close();
+
+   PutObjectRequest putObjectRequest = new PutObjectRequest( awsS3AudioBucket, fileName, file );
+
+   if ( enablePublicReadAccess ) {
+    putObjectRequest.withCannedAcl( CannedAccessControlList.PublicRead );
+   }
+
+   amazonS3.putObject( putObjectRequest );
+   file.delete();
+   return Optional.of( multipartFile );
+
+  } catch ( IOException | AmazonServiceException error ) {
+   logger.error( error.getMessage() );
+   return Optional.empty();
+  }
+ }
+
+ @Async
+ public void deleteFileFromS3 (String fileName) {
+  try {
+   DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest( awsS3AudioBucket, fileName );
+   amazonS3.deleteObject( deleteObjectRequest );
+
+  } catch ( AmazonServiceException error ) {
+   logger.error( error.getMessage() );
+  }
+ }
+}
---
+++ backend/src/main/java/org/opensource/schulaltas/service/StorageService.java
@@ -0,0 +1,40 @@
+package org.opensource.schulaltas.service;
+
+import org.opensource.schulaltas.config.AWSConfig;
+import org.opensource.schulaltas.model.page.Attachment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Optional;
+
+@Service
+public class StorageService {
+
+ private final AWSService awsService;
+ private final AWSConfig awsConfig;
+
+ @Autowired
+ public StorageService (AWSService awsService, AWSConfig awsConfig) {
+  this.awsService = awsService;
+  this.awsConfig = awsConfig;
+ }
+
+ public Optional<Attachment> save (MultipartFile file) {
+  Optional<MultipartFile> returningFile = awsService.uploadFileToS3( file, true );
+  if ( returningFile.isPresent() ) {
+   String fileUrl =
+           "https://" + awsConfig.getAWSS3AudioBucket() + ".s3.amazonaws.com/" + file.getOriginalFilename();
+   return Optional.of( Attachment.builder()
+                               .fileName( file.getOriginalFilename() )
+                               .url( fileUrl )
+                               .type( file.getContentType() )
+                               .build() );
+  }
+  return Optional.empty();
+ }
+
+ public void deleteFileById (String fileName) {
+  awsService.deleteFileFromS3( fileName );
+ }
+}
