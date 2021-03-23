05574028d31f53870c411d973f9fa4db88847cf2
dennisjauernig <dennis@jauernig.email>
2021-03-23 23:27:22 +0100

    :building_construction: Add typeDto to return types and counts

---
+++ backend/src/main/java/org/opensource/schulaltas/controller/model/TypeDto.java
@@ -0,0 +1,16 @@
+package org.opensource.schulaltas.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder (toBuilder = true)
+public class TypeDto {
+
+ private String type;
+ private int count;
+}
