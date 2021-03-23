af23274aed41133fcc10418bb532d9ae843feb52
dennisjauernig <dennis@jauernig.email>
2021-03-23 09:14:30 +0100

    :passport_control: Save jwt to sessionStorage, on reload check on expiration

--- frontend/src/contexts/AuthProvider.js
+++ frontend/src/contexts/AuthProvider.js
@@ -1,3 +1,4 @@
+import decode from 'jwt-decode';
 import { node } from 'prop-types';
 import { createContext, useContext, useEffect, useState } from 'react';
 import axiosConfig from '../config/axiosConfig';
@@ -7,9 +8,29 @@ export const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState('');
 
+  const isTokenExpired = (tokenToTest) => {
+    try {
+      const decodedToken = decode(tokenToTest);
+      if (decodedToken.exp < Date.now() / 1000) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
+  };
+
+  useEffect(() => {
+    const possibleToken = sessionStorage.getItem('schulatlas');
+    if (possibleToken && !isTokenExpired(possibleToken)) {
+      setToken(possibleToken);
+    }
+  }, []);
+
   useEffect(() => {
     if (token) {
       axiosConfig.setAuthToken(token);
+      sessionStorage.setItem('schulatlas', token);
     }
   }, [token]);
 
