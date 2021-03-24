ef87113460c42496bac478e2df7ac9f8d9de647a
dennisjauernig <dennis@jauernig.email>
2021-03-24 20:01:35 +0100

    :recycle: Refactor components to styled-components

--- frontend/src/components/links/NavigationLink.js
+++ frontend/src/components/links/NavigationLink.js
@@ -2,15 +2,13 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components/macro';
 
 const NavigationLink = styled(NavLink)`
-  > a {
-    outline: none;
-    text-decoration: none;
-    color: white;
-    transition: var(--transition-color);
+  outline: none;
+  text-decoration: none;
+  color: white;
+  transition: var(--transition-color);
 
-    :hover {
-      color: var(--color-gold-crayola);
-    }
+  :hover {
+    color: var(--color-gold-crayola);
   }
 `;
 
