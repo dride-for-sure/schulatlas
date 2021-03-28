b340d4e30f368881317b86ec12d3f57efcb62abc
dennisjauernig <dennis@jauernig.email>
2021-03-28 23:25:46 +0200

    :lipstick: Add margin between list elements

--- frontend/src/components/lists/UnorderedList.js
+++ frontend/src/components/lists/UnorderedList.js
@@ -4,6 +4,10 @@ const UnorderedList = styled.ul`
   list-style-position: inside;
   margin: var(--default-margin) 0;
   text-transform: capitalize;
+
+  > li + li {
+    margin-top: calc(var(--default-margin) * 0.15);
+  }
 `;
 
 export default UnorderedList;
