e6c1cb67c7a62adc13e9324dc8a0cf0b8ef1ddca
dennisjauernig <dennis@jauernig.email>
2021-03-28 20:40:48 +0200

    :sparkles: Add slug component

---
+++ frontend/src/components/assemblies/Slug.js
@@ -0,0 +1,23 @@
+import { func, string } from 'prop-types';
+import Input from '../form/Input';
+import Label from '../form/Label';
+
+export default function Slug({ slug, onChange }) {
+  return (
+    <>
+      <Label>Slug</Label>
+      <Input
+        align="left"
+        id="slug"
+        type="text"
+        placeholder="Enter page slug"
+        value={slug || ''}
+        onChange={onChange} />
+    </>
+  );
+}
+
+Slug.propTypes = {
+  slug: string.isRequired,
+  onChange: func.isRequired,
+};
