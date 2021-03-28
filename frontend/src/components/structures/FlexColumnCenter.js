35e019b58fd1e8440cc7c775689511148bf5c322
dennisjauernig <dennis@jauernig.email>
2021-03-28 18:18:06 +0200

    :sparkles: Add structure components

---
+++ frontend/src/components/structures/FlexColumnCenter.js
@@ -0,0 +1,9 @@
+import { css } from 'styled-components/macro';
+
+const FlexColumnCenter = css`
+  display:flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export default FlexColumnCenter;
---
+++ frontend/src/components/structures/FlexRowCenter.js
@@ -0,0 +1,8 @@
+import { css } from 'styled-components/macro';
+
+const FlexRowCenter = css`
+  display:flex;
+  justify-content: center;
+`;
+
+export default FlexRowCenter;
