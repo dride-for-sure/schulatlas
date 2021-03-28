93727009293dc892586bb7465c645e1c62d9e221
dennisjauernig <dennis@jauernig.email>
2021-03-28 23:21:49 +0200

    :adhesive_bandage: Fix page sorting

--- frontend/src/common/sortPages.js
+++ frontend/src/common/sortPages.js
@@ -1,5 +1,11 @@
 const sortPages = (pages) =>
   pages.sort((a, b) => {
+    if (a.landingPage) {
+      return 1;
+    }
+    if (b.landingPage) {
+      return -1;
+    }
     if (a.slug.toLowerCase() < b.slug.toLowerCase()) {
       return -1;
     }
