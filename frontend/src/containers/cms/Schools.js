2022b3c03b8a4db3ada76372393ab9e4ec1954f3
dennisjauernig <dennis@jauernig.email>
2021-03-30 00:31:50 +0200

    :sparkles: Add all changes regarding pagination, sorting, filtering

--- frontend/src/containers/cms/Schools.js
+++ frontend/src/containers/cms/Schools.js
@@ -1,19 +1,22 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useHistory, useLocation, useParams } from 'react-router-dom';
 import styled from 'styled-components/macro';
+import getSearchParams from '../../common/searchParams';
 import GridSideBar from '../../components/grid/cms/GridSideBar';
 import Header from '../../components/header/cms/Header';
 import SchoolList from '../../components/lists/cms/school/SchoolList';
 import EditSchool from '../../components/parts/cms/EditSchool/EditSchool';
 import SideBar from '../../components/parts/cms/SideBar';
-import FlexRowCenter from '../../components/structures/FlexRowCenter';
-import { getSchoolByNumber, getSchoolByType, listSchools, listTypes } from '../../services/api/private/schoolApiService';
+import FlexRowCenter from '../../components/structures/_FlexRowCenter';
+import { getSchoolByNumber, getSchoolsByType, listSchools, listTypes } from '../../services/api/private/schoolApiService';
 
 export default function SchoolsOverview() {
   const [types, setTypes] = useState('');
   const [schools, setSchools] = useState('');
   const [school, setSchool] = useState('');
   const { type, number } = useParams();
+  const { search } = useLocation();
+  const history = useHistory();
 
   const clearStates = () => {
     setSchools('');
@@ -23,7 +26,7 @@ export default function SchoolsOverview() {
   const handleParamUpdates = () => {
     clearStates();
     if (type) {
-      getSchoolByType(type)
+      getSchoolsByType(type)
         .then(setSchools)
         .catch((error) => console.log(error));
     } else if (number) {
@@ -31,7 +34,8 @@ export default function SchoolsOverview() {
         .then(setSchool)
         .catch((error) => console.log(error));
     } else {
-      listSchools()
+      const params = getSearchParams(search);
+      listSchools(params.page, params.size, params.sort, params.direction)
         .then(setSchools)
         .catch((error) => console.log(error));
     }
@@ -43,9 +47,30 @@ export default function SchoolsOverview() {
       .catch((error) => console.log(error));
   };
 
+  const handlPagination = (whichWay) => {
+    const params = getSearchParams(search);
+    let targetPage = 0;
+    if (whichWay === 'last') {
+      targetPage = (params.page === null || params.page <= 0)
+        ? 0
+        : params.page - 1;
+    } else {
+      targetPage = (params.page === null || params.page >= schools.totalPages - 1)
+        ? schools.totalPages - 1
+        : params.page + 1;
+    }
+    history.push(`?page=${targetPage}&size=${params.size}&sort=${params.sort}&direction=${params.direction}`);
+  };
+
+  const toggleSort = (sortType) => {
+    const params = getSearchParams(search);
+    const direction = params.direction === 'asc' ? 'desc' : 'asc';
+    history.push(`?page=${params.page}&size=${params.size}&sort=${sortType}&direction=${direction}`);
+  };
+
   useEffect(() => {
     handleParamUpdates();
-  }, [type, number]);
+  }, [type, number, search]);
 
   useEffect(() => {
     getTypeList();
@@ -57,7 +82,14 @@ export default function SchoolsOverview() {
       <Container>
         <GridSideBar>
           {types && <SideBar types={types} />}
-          {schools && <SchoolList schools={schools} />}
+          {schools && (
+          <SchoolList
+            type={type}
+            schools={schools}
+            onPagination={handlPagination}
+            toggleSort={toggleSort}
+             />
+          )}
           {school && <EditSchool school={school} />}
         </GridSideBar>
       </Container>
