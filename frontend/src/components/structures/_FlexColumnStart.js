330b73b276cedae1f8bfe3cbccf05478f4e1959e
dennisjauernig <dennis@jauernig.email>
2021-04-02 16:47:56 +0200

    :lipstick: Add another flex component

---
+++ frontend/src/components/structures/_FlexColumnStart.js
@@ -0,0 +1,9 @@
+import { css } from 'styled-components/macro';
+
+const FlexColumnStart = css`
+  display:flex;
+  flex-direction: column;
+  justify-content: start;
+`;
+
+export default FlexColumnStart;
