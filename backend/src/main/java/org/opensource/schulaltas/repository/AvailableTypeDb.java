dcff9550d9e19c50c0aef50a8461a3ed025ec5a8
dennisjauernig <dennis@jauernig.email>
2021-04-05 17:00:28 +0200

    :card_file_box: Add findAllByName to availableTypes interface

--- backend/src/main/java/org/opensource/schulaltas/repository/AvailableTypeDb.java
+++ backend/src/main/java/org/opensource/schulaltas/repository/AvailableTypeDb.java
@@ -1,6 +1,8 @@
 package org.opensource.schulaltas.repository;
 
 import org.opensource.schulaltas.model.school.AvailableType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -8,4 +10,6 @@ import java.util.List;
 public interface AvailableTypeDb extends PagingAndSortingRepository<AvailableType, String> {
  @Override
  List<AvailableType> findAll ();
+
+ Page<AvailableType> findAllByNameContainingIgnoreCase (String name, Pageable pageable);
 }
