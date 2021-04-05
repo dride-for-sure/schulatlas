6bbdc563d7024c14f2e61adfb01df7499260384e
dennisjauernig <dennis@jauernig.email>
2021-04-05 16:46:42 +0200

    :sparkles: Add search & delete components

--- frontend/src/components/icons/Delete.js
+++ frontend/src/components/icons/Delete.js
@@ -1,12 +1,18 @@
+import { bool } from 'prop-types';
 import styled from 'styled-components/macro';
+import DeleteDark from '../../resources/images/DeleteDark.png';
 import DeleteWhite from '../../resources/images/DeleteWhite.png';
 
-export default function Delete() {
+export default function Delete({ dark }) {
   return (
-    <Image src={DeleteWhite} alt="Delete" />
+    <Image src={dark ? DeleteDark : DeleteWhite} alt="Delete" />
   );
 }
 
 const Image = styled.img`
   height: 11px;
 `;
+
+Delete.propTypes = {
+  dark: bool.isRequired,
+};
