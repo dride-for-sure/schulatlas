8d903636a1e9263a3c5f76cae2ef405862d636db
dennisjauernig <dennis@jauernig.email>
2021-04-05 16:45:50 +0200

    :sparkles: Escape slug to be url conform

--- frontend/src/common/slug.js
+++ frontend/src/common/slug.js
@@ -1,3 +1,9 @@
 export const prettifySlug = (slug) => slug.replaceAll('-', ' ');
 
-export const escapeSlug = (slug) => slug.trim().replaceAll(' ', '-');
+export const escapeSlug = (slug) => slug.trim().replace(/[^a-zA-Z0-9-_]/g, '');
+
+/**
+ * TODO:
+ * On Enter -> fulltext
+ * Tests schreiben
+ */
