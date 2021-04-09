8299f11f2ab398258433af70b6e3617fd54a78d3
dennisjauernig <dennis@jauernig.email>
2021-04-09 11:21:32 +0200

    :sparkles: Add text handling/styling components

---
+++ frontend/src/components/text/_TextEllipsis.js
@@ -0,0 +1,9 @@
+import { css } from 'styled-components/macro';
+
+const TextEllipsis = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+export default TextEllipsis;
