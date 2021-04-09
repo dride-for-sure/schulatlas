27161ebc76060360bc79f8874682f91aef4ae6d2
dennisjauernig <dennis@jauernig.email>
2021-04-09 14:05:48 +0200

    :green_heart: Update dockerfile

--- backend/src/main/java/org/opensource/schulaltas/controller/ReactController.java
+++ backend/src/main/java/org/opensource/schulaltas/controller/ReactController.java
@@ -3,13 +3,11 @@ package org.opensource.schulaltas.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
 public class ReactController {
 
  @RequestMapping (value = { "/", "/*", "/cms/**" })
- public String forward (HttpServletRequest httpServletRequest) {
-  return "forward:/index.html";
+ public String forward () {
+  return "index.html";
  }
 }
