1dc7366a021062a02297afeb05f8b3277b81e8dd
dennisjauernig <dennis@jauernig.email>
2021-04-09 11:12:14 +0200

    :bug: Convert to lowercase

--- frontend/src/common/slug.js
+++ frontend/src/common/slug.js
@@ -1,9 +1,3 @@
 export const prettifySlug = (slug) => slug.replaceAll('-', ' ');
 
-export const escapeSlug = (slug) => slug.trim().replace(/[^a-zA-Z0-9-_]/g, '');
-
-/**
- * TODO:
- * On Enter -> fulltext
- * Tests schreiben
- */
+export const escapeSlug = (slug) => slug.trim().replace(/[^a-zA-Z0-9-_]/g, '').toLowerCase();
