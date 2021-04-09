8c812dcbfd2bb0e650201116b266f2c4972273e9
dennisjauernig <dennis@jauernig.email>
2021-04-09 11:36:22 +0200

    :sparkles: Add getLandingPage endpoint

--- frontend/src/services/api/public/pageApiService.js
+++ frontend/src/services/api/public/pageApiService.js
@@ -2,8 +2,10 @@ import axios from 'axios';
 
 const PAGE_API_BASE_URL = '/api/v1/page';
 
-const getPageBySlug = (slug) => axios
+export const getPageBySlug = (slug) => axios
   .get(`${PAGE_API_BASE_URL}/slug/${slug}`)
   .then((response) => response.data);
 
-export default getPageBySlug;
+export const getLandingPage = () => axios
+  .get(`${PAGE_API_BASE_URL}/landingpage`)
+  .then((response) => response.data);
