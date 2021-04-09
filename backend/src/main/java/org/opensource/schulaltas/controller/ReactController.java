55af7291cfe0bed6f1c2d42c030c5e0d6408633b
Dennis Jauernig <10451521+dride-for-sure@users.noreply.github.com>
2021-04-09 14:14:25 +0200

    Update reactController (#96)
    * :green_heart: Update dockerfile
    
    * :green_heart: Update dockerfile
    
    * :green_heart: Update dockerfile
    
    * :green_heart: Update reactController
    
    * :green_heart: Update reactController

--- backend/src/main/java/org/opensource/schulaltas/controller/ReactController.java
+++ backend/src/main/java/org/opensource/schulaltas/controller/ReactController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ReactController {
 
- @RequestMapping (value = { "/", "/*", "/cms/**" })
+ @RequestMapping (value = { "/" })
  public String forward () {
   return "index.html";
+ }
+
+ @RequestMapping (value = { "/*" })
+ public String forward () {
+  return "/";
+ }
+
+ @RequestMapping (value = { "/cms/**" })
+ public String forward () {
+  return "/";
  }
 }
